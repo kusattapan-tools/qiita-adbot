6a6985cbd671f685c1161c8df52d3652bd83dc9e
kusattapan0 <discordmainaccount@protonmail.com>
2021-10-08 06:46:44 +0900

    とりあえずうごいたのでおｋ

--- qiita-adbot.py
+++ qiita-adbot.py
@@ -5,13 +5,53 @@ import requests
 from lxml import html
 from time import sleep
 import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-email = input('Qiitaのメールアドレス:')
-password = input('Qiitaのパスワード:')
+combos = ['daratmp+hxwva@gmail.com', 'whentmp+jnfm6@gmail.com', 'joneltmp+c21jr@gmail.com', 'sisatmp+du6zv@gmail.com', 'dotatmp+mzjg9@gmail.com', 'puritmp+k0mmo@gmail.com', 'ezratmp+muf6q@gmail.com']
 tags = {}
 
-subject = open('subject.txt', 'r')
-text = open('text.txt', 'r')
+subject = "ポイ活の勧め。海外ポイントサイト・DropGCの紹介。"
+text = """
+皆さんはポイ活をしていますか？メディアで見たり聞いたりする機会もあると思います。
+
+ポイ活とはポイント活動の略称です。ポイントを貯めれば、日々の生活をお得にできます。現金化する事も可能で、ポイントには夢が一杯あります！お小遣いが少しでも増えたら嬉しいですよね。
+
+私自身はポイ活を2017年くらいからしています。決して怪しい活動ではないのでご安心を。
+
+では、ポイ活はどうやって始められるのでしょうか。難しい手続きは一切ありません。ポイントサイトに登録するか、アプリを使うだけです。スマホやパソコンで簡単にお小遣い稼ぎができます。
+
+私が登録しているポイントサイトをご紹介します。それは「DropGC」です。
+
+大量広告で高還元の多いDropGC。10ポイント＝1円という特殊な設定から「実は大した金額じゃないのでは・・」と疑われて損しているように思います。同じ広告を比較すると平均的にモッピーやげん玉を上回っています。
+
+DropGCの利用に慣れてきたら、注目したいのが友達紹介制度です。家族や知人、ブログやツイッターなどで紹介することができます。紹介するとポイントがついたり、利用してくれた案件の数十%がずっと還元されるシステムです。もちろん友達のポイントが減ることはありません。
+とても魅力的なこの制度を生かしてみましょう。
+
+広告量やゲーム量が多く、最も少額の100円から無料で交換できるので、とりあえず換金してみたいなら1番手軽にお小遣いにできます。
+0歳以上から登録利用無料です。
+
+どのサイトも登録や利用は無料なので、組み合わせて利用するのもオススメです。高報酬なポイントサイトが多いので、たくさん登録しすぎると使いきれない、ポイントが分散して稼ぎにくいと感じます。DropGC1本を極めるか、メインサイト＋比較サイトが少しあると便利です。
+
+この業界の中では珍しく米国に本拠を置く財団が運営しています。
+非常に安全性の高いサイトです
+
+DropGCでは、全ページでSSL/TLSを導入しています。
+
+SSL/TLSというのは、情報を暗号化して送受信することで第三者から大切な情報を勝手に見られないようにする技術です。
+
+ちなみに、SSL/TLSの導入は、サイトURL「http」の後に「ｓ」があることで確認することができ、DropGCでもバッチリ確認できます。
+
+全然稼げないポイントサイトも多いので、適当に選んでいるとまったく稼げないことが多いです。
+
+しかし、DropGCはしっかり使えば、かなりの金額を稼ぐことも可能です。
+
+https://dropgc.gift
+【↑此方のリンクからDropGCに登録可能です】
+"""
+
+driver = webdriver.Chrome()
 
 def getTags():
     print('タグを収集中....')
@@ -22,23 +62,17 @@ def getTags():
     return tags
 def login():
     print('アカウントにログイン中...')
-    s = requests.session()
+    driver.get('https://qiita.com/login')
 
-    response = s.get('https://qiita.com/login')
-    tree = html.fromstring(response.content)
-    csrfToken = tree.xpath('//input[@name="authenticity_token"]/@value')
-    if(type(csrfToken) == list):
-        csrfToken = csrfToken[0]
+    cred = random.choice(combos)
 
-    print(csrfToken)
-    response = s.post('https://qiita.com/login', data={ "authenticity_token": csrfToken, "identity": email, "password": password, "commit": "Qiita にログイン" }, headers={ "accept-language": "ja,en-US;q=0.9,en;q=0.8" })
-    print(response.status_code)
-    if response.status_code != 302:
-        print('ログインに失敗しました')
-        exit()
-    print("ログイン成功"+response.headers.get('set-cookie'))
-    return response.headers.get('set-cookie')
-def post (tags, session):
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//input[@name="identity"]')))
+    driver.find_element_by_xpath('//input[@name="identity"]').send_keys(cred)
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//input[@name="password"]')))
+    driver.find_element_by_xpath('//input[@name="password"]').send_keys(cred)
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//input[@name="commit"]')))
+    driver.find_element_by_xpath('//input[@name="commit"]').click()
+def post (tags):
     t = []
     for i in range(5):
         tag = random.choice(tags)
@@ -46,16 +80,37 @@ def post (tags, session):
             i = i - 1
         else:
             t.append(tag)
-    print('タグを決定しました'+t)
-    response = requests.post('https://qiita.com/graphql', headers={ "cookie": session, "origin": "https://qiita.com" })
-    tree = html.fromstring(response.content)
+    tagStr = ''
+    for tagg in t:
+        tagStr = tagStr + tagg + " "
+    driver.execute_script('return location.href="https://qiita.com/drafts/new";')
+
+    print('タグを決定しました'+tagStr)
+
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@class="css-12fqrnh e1aqgsb41"]')))
+    driver.find_element_by_xpath('//input[@class="css-12fqrnh e1aqgsb41"]').send_keys(subject)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//textarea[@class="css-avadu4"]')))
+    driver.find_element_by_xpath('//textarea[@class="css-avadu4"]').send_keys(text)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@class="css-19fi7g8 ed5758y1"]')))
+    driver.find_element_by_xpath('//input[@class="css-19fi7g8 ed5758y1"]').send_keys(tagStr)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//button[@class="e9bkwgs1 css-8voofm e1rb7ucl0"]')))
+    driver.find_element_by_xpath('//button[@class="e9bkwgs1 css-8voofm e1rb7ucl0"]').click()
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//button[@class="css-lc87ok e1rb7ucl0"]')))
+    if(len(driver.find_elements_by_xpath('//input[@class="css-15ro776"]')) > 0):
+        for check in driver.find_elements_by_xpath('//input[@class="css-15ro776"]'):
+            check.click()
+
+    driver.find_element_by_xpath('//button[@class="css-lc87ok e1rb7ucl0"]').click()
     print('記事の投稿が完了しました')
 
 
 if __name__ == '__main__':
     tags = getTags()
-    session = login()
+    login()
     
-    #while True:
-     #   post(tags, session)
-      #  sleep(0.5)
+    while True:
+        try:
+            post(tags)
+        except:
+            login()
+        sleep(0.5)
